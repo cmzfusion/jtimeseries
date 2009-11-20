8a1fcdf37bcef4627c7e463ec2eae097e35dac7c
janick388 <janick388@1f22a742-3ce0-4918-9abc-d7d91150c296>
2009-11-20 19:00:22 +0000

    Correct packages for demo, and add demo module to main pom

--- jtimeseries-demo/src/main/java/com/od/jtimeseries/demo/JTimeSeriesDemo.java
+++ jtimeseries-demo/src/main/java/com/od/jtimeseries/demo/JTimeSeriesDemo.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with JTimeseries.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.od.metrics4j.demo;
+package com.od.jtimeseries.demo;
 
 import com.od.jtimeseries.JTimeSeries;
 import com.od.jtimeseries.source.ValueRecorder;

9894773772981276f40b5cead1d4ae78e7d4ee3a
janick388 <janick388@1f22a742-3ce0-4918-9abc-d7d91150c296>
2010-06-11 13:56:26 +0000

    correct start/stop scripts for server and agent

--- jtimeseries-server/src/etc/start.sh
+++ jtimeseries-server/src/etc/start.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+#
+# Start the jtimeseries-server
+
 java -Xmx256M -cp ./;./jtimeseries-server-${version}.jar;./jtimeseries-component-${version}.jar;./jtimeseries-${version}.jar;./jfreechart-1.0.9.jar;./aopalliance-1.0.jar;./jcommon-1.0.12.jar;./commons-logging-1.1.1.jar;./jcommon-1.0.12.jar;./jmxtools-1.2.1.jar;./spring-beans-2.5.6.jar;./spring-context-2.5.6.jar;./spring-core-2.5.6.jar com.od.jtimeseries.server.JTimeSeriesServer
-pause

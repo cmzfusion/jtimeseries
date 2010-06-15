f3c964200c236d05f1a3b70e70389c4cc9874fc6
janick388 <janick388@1f22a742-3ce0-4918-9abc-d7d91150c296>
2010-06-15 16:14:25 +0000

    Allow expansion of series id and description based on input handler pattern

--- jtimeseries-agent/src/main/java/com/od/jtimeseries/agent/input/DoubleRegexValueHandler.java
+++ jtimeseries-agent/src/main/java/com/od/jtimeseries/agent/input/DoubleRegexValueHandler.java
@@ -7,7 +7,10 @@ import com.od.jtimeseries.util.logging.LogMethods;
 import com.od.jtimeseries.util.logging.LogUtils;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,34 +19,33 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Time: 17:34:21
  *
  * parse a numeric value and use a valueRecorder to capture the value into one or more series using the CaptureFunctions specified
+ * The destinationSeriesPath specified may contain references to capture groups from the pattern, which will be expanded
+ * to determine
  */
 public class DoubleRegexValueHandler implements RegexValueHandler {
 
     protected static LogMethods logMethods = LogUtils.getLogMethods(DoubleRegexValueHandler.class);
 
     private AtomicBoolean errorLogged = new AtomicBoolean();
-    private ValueRecorder valueRecorder;
+    private Map<String, ValueRecorder> expandedSeriesToValueRecorderMap = new HashMap<String, ValueRecorder>();
+    private TimeSeriesContext rootContext;
     private String destinationSeriesPath;
+    private String seriesDescription;
     private int regExCaptureGroup;
+    private CaptureFunction[] captureFunctions;
 
     public DoubleRegexValueHandler(TimeSeriesContext rootContext, String destinationSeriesPath, String seriesDescription, int regExCaptureGroup, CaptureFunction... captureFunctions) {
+        this.rootContext = rootContext;
         this.destinationSeriesPath = destinationSeriesPath;
+        this.seriesDescription = seriesDescription;
         this.regExCaptureGroup = regExCaptureGroup;
+        this.captureFunctions = captureFunctions;
 
-        createSeries(rootContext, destinationSeriesPath, seriesDescription, captureFunctions);
-    }
-
-    private void createSeries(TimeSeriesContext rootContext, String destinationSeriesPath, String seriesDescription, CaptureFunction... captureFunctions) {
-        logMethods.logInfo("Creating series for path " + destinationSeriesPath);
-        if ( rootContext.findValueSources(destinationSeriesPath).getNumberOfMatches() > 0 ) {
-            throw new RuntimeException("There is already a series set up with path " + destinationSeriesPath + ". Cannot set up this value handler");
-        }
-
-        valueRecorder = rootContext.createValueRecorderSeries(destinationSeriesPath, seriesDescription, captureFunctions);
-        logMethods.logInfo("Creating valueRecorder for path " + destinationSeriesPath);
+        logMethods.logInfo("Creating DoubleRegexValueHandler for path " + destinationSeriesPath);        
     }
 
     public void parseInputValue(Matcher m) {
+        logMethods.logDebug("initial group count: " + m.groupCount());
         if ( m.groupCount() < regExCaptureGroup) {
             logGroupError();
         } else {
@@ -52,10 +54,23 @@ public class DoubleRegexValueHandler implements RegexValueHandler {
     }
 
     private void parseAndRecordValue(Matcher m) {
+        String source = m.group(0);
+
+        //target series path is the destinationSeriesPath with any capturing groups replaced
+        String expandedSeriesPath = m.replaceAll(destinationSeriesPath);
+        m.reset();
+        m.find();
+
+        String expandedDescription = m.replaceAll(seriesDescription);
+        m.reset();
+        m.find();
+
+        ValueRecorder v = getOrCreateSeries(expandedSeriesPath, expandedDescription);
         String value = m.group(regExCaptureGroup);
+        logMethods.logDebug("DoubleRegexValueHandler processing value [" + value + "] for series " + expandedSeriesPath);
         try {
             Double d = Double.parseDouble(value);
-            valueRecorder.newValue(d);
+            v.newValue(d);
         } catch ( NumberFormatException nfe) {
             if ( ! errorLogged.getAndSet(true)) {
                 //don't want to log this for every line of input
@@ -63,6 +78,22 @@ public class DoubleRegexValueHandler implements RegexValueHandler {
                         " for group " + regExCaptureGroup + " appears not to be numeric");
             }
         }
+    }
+
+    private ValueRecorder getOrCreateSeries(String expandedSeriesPath, String expandedDescription) {
+        ValueRecorder result = expandedSeriesToValueRecorderMap.get(expandedSeriesPath);
+        if ( result == null ) {
+            logMethods.logInfo("Creating series for path " + expandedSeriesPath);
+            if ( rootContext.findValueSources(expandedSeriesPath).getNumberOfMatches() > 0 ) {
+                throw new RuntimeException("There is already a series set up with path " + expandedSeriesPath + ". Cannot set up this value handler");
+            }
+
+            result = rootContext.createValueRecorderSeries(expandedSeriesPath, expandedDescription, captureFunctions);
+            logMethods.logInfo("Creating valueRecorder for path " + expandedSeriesPath);
+
+            expandedSeriesToValueRecorderMap.put(expandedSeriesPath, result);
+        }
+        return result;
     }
 
     private void logGroupError() {

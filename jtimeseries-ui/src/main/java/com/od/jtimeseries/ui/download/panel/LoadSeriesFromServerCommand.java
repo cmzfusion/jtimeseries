79692f80d8ae1f0b703d3a69d759d44a6c380204
janick388 <janick388@1f22a742-3ce0-4918-9abc-d7d91150c296>
2011-06-21 09:21:12 +0000

    Downgrade error to warning

--- jtimeseries-ui/src/main/java/com/od/jtimeseries/ui/download/panel/LoadSeriesFromServerCommand.java
+++ jtimeseries-ui/src/main/java/com/od/jtimeseries/ui/download/panel/LoadSeriesFromServerCommand.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2011 (nick @ objectdefinitions.com)
+ *
+ * This file is part of JTimeseries.
+ *
+ * JTimeseries is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JTimeseries is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JTimeseries.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.od.jtimeseries.ui.download.panel;
 
 import com.od.jtimeseries.context.TimeSeriesContext;
@@ -28,7 +46,7 @@ public class LoadSeriesFromServerCommand extends SwingCommand<TimeSeriesServer, 
                 public void error(Task task, Throwable error) {
                     TimeSeriesServer s = (TimeSeriesServer)task.getParameters();
                     s.setConnectionFailed(true);
-                    logMethods.logError("Failed to load series from server " + task.getParameters(), error);
+                    logMethods.logWarning("Failed to load series from server " + task.getParameters(), error);
                 }
 
                 public void success(Task task) {

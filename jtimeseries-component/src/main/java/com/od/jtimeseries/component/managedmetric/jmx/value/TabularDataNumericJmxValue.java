aabc23fe0ae10a153f2a1b0023364a21f151d5c6
janick388 <janick388@1f22a742-3ce0-4918-9abc-d7d91150c296>
2012-01-17 12:56:56 +0000

    Improve exception handling and logging for jmx metric values

--- jtimeseries-component/src/main/java/com/od/jtimeseries/component/managedmetric/jmx/value/TabularDataNumericJmxValue.java
+++ jtimeseries-component/src/main/java/com/od/jtimeseries/component/managedmetric/jmx/value/TabularDataNumericJmxValue.java
@@ -4,10 +4,7 @@ import com.od.jtimeseries.timeseries.function.aggregate.AggregateFunction;
 
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
-import javax.management.openmbean.CompositeData;
-import javax.management.openmbean.CompositeDataSupport;
-import javax.management.openmbean.TabularDataSupport;
-import java.util.Map;
+import javax.management.openmbean.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,7 +13,7 @@ import java.util.Map;
  * Time: 15:25
  * To change this template use File | Settings | File Templates.
  */
-public class TabularDataNumericJmxValue extends JmxValue {
+public class TabularDataNumericJmxValue extends SimpleJmxValue {
 
     private String tabularDataKey;
 
@@ -38,11 +35,41 @@ public class TabularDataNumericJmxValue extends JmxValue {
      * @throws Exception
      */
     protected void readValueFromBean(MBeanServerConnection jmxConnection, AggregateFunction f, ObjectName beanName) throws Exception {
-        TabularDataSupport tabularDataSupport = ((TabularDataSupport)jmxConnection.getAttribute(new ObjectName(getObjectName()), getAttribute()));
+        checkBeanExists(jmxConnection, beanName);
+        Object attribute = safelyGetAttribute(jmxConnection, beanName);
+
+        if ( ! (attribute instanceof TabularData)) {
+            raiseValueException("The attribute was not of type TabularData");
+        }
+
+        TabularData tabularData = (TabularData) attribute;
         Object[] key = {tabularDataKey};
-        CompositeData compositeData = tabularDataSupport.get(key);
-        String value = (String) compositeData.get("value");
-        f.addValue(Double.valueOf(value));
+
+        if ( ! tabularData.containsKey(key)) {
+            raiseValueException("No value is available for the key Object[] {" + tabularDataKey + "}");
+        }
+
+        CompositeData compositeData = null;
+        try {
+            compositeData = tabularData.get(key);
+        } catch ( InvalidKeyException k) {
+            raiseValueException("The TabularDataKey " + tabularDataKey + " was invalid", k);
+        }
+
+        if ( compositeData == null) {
+            raiseValueException("Even after checking the TabularDataKey " + tabularDataKey + " existed, we were still return a null value");
+            //key asynchronously removed on remote server?, probably possible, but never seen it
+        }
+
+        Object value = getValueFromCompositeData(compositeData, "value");
+        if ( value == null ) {
+            raiseValueException("The value attribute of the CompositeDataItem with tabular key " + tabularDataKey + " was null");
+        }
+
+        Double v = convertToDouble(value);
+
+        //yippee! after all this checking we actually have a value!
+        f.addValue(v);
     }
 
 

96e0489b3330a8a5b7d2c1f7d0537c46ad5162a0
Nick Ebbutt <Nick.Ebbutt@partner.commerzbank.com>
2018-09-21 10:03:00 +0100

    EBD-12273 Fix LRU cache in JTimeseries-Server

--- jtimeseries-component/src/test/java/com/od/jtimeseries/component/util/cache/TestLRUCache.java
+++ jtimeseries-component/src/test/java/com/od/jtimeseries/component/util/cache/TestLRUCache.java
@@ -57,7 +57,7 @@ public class TestLRUCache extends TestCase {
 
     private void waitForExpansion() {
         try {
-            Thread.sleep(100);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
